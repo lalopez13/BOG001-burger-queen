49b490e0c62e0acccaa4a686dfa20776e2cae58d
Caro <cslondo90@gmail.com>
2020-10-20 13:33:25 -0500

    (Config): Firebase & Firestore plus menu.json

---
+++ src/firebase.js
@@ -0,0 +1,25 @@
+import firebase from 'firebase'
+import 'firebase/firestore'
+
+
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCb1nKXYDdLynUjgXBIHkQlV8qVbDxQ_mQ",
+    authDomain: "bqueen-app.firebaseapp.com",
+    databaseURL: "https://bqueen-app.firebaseio.com",
+    projectId: "bqueen-app",
+    storageBucket: "bqueen-app.appspot.com",
+    messagingSenderId: "1051333716802",
+    appId: "1:1051333716802:web:8eaae2ffe0862a4163b6db",
+    measurementId: "G-FMCPZ746CN"
+  };
+
+  firebase.initializeApp(firebaseConfig);
+
+  let db = firebase.firestore()
+
+
+  export default {
+    firebase, db
+  }
--- src/views/chef/chefView.js
+++ src/views/chef/chefView.js
@@ -5,9 +5,10 @@ import './chef.scss'
 function chefView() {
 
   return (
-    <div>
-      <h1 className="title">This is the chef view</h1>
-    </div>
+    
+    <pendingList>
+      <button>Pedido entrante 1    001</button>
+    </pendingList>
   );
 }
 
