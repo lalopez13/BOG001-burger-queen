3333a7aa371737b249a71d49824481b86f9ae11c
lalopez13 <63772079+lalopez13@users.noreply.github.com>
2020-10-20 10:55:42 -0500

    organizacion de archivos/instalacion de sass

---
+++ src/views/chef/chefView.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './chef.scss'
+
+
+function chefView() {
+
+  return (
+    <div>
+      <h1 className="title">This is the chef view</h1>
+    </div>
+  );
+}
+
+export default chefView;
