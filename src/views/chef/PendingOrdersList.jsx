c5c1ccb1048a99ca47ea8751e6612be0b7976cfe
Caro <cslondo90@gmail.com>
2020-11-11 11:16:18 -0500

    (WaiterViwe:) Notifications modal finished

--- src/views/chef/PendingOrdersList.jsx
+++ src/views/chef/PendingOrdersList.jsx
@@ -1,32 +1,31 @@
-import React, { useState , Fragment } from "react";
+import React, { useState, Fragment } from "react";
 import UseOrdersPending from "./UseOrdersPending";
 import SinglePendingOrderComponent from "./SinglePendingOrderComponent";
 import SingleOrderContext from "./SingleOrderContext.jsx";
-import DefaultMessage from './DefaultMessage'
-
+import DefaultMessage from "./DefaultMessage";
 
 const PendingOrders = () => {
   const currentOrdersList = UseOrdersPending();
   const [singleOrderData, setSingleOrderData] = useState();
   const [selectedOrder, setSelectedOrder] = useState();
 
-
-  
-
   const currentActiveOrder = (orderData) => {
     setSingleOrderData(orderData);
-    setSelectedOrder(orderData.orderId);    
-  }
+    setSelectedOrder(orderData.orderId);
+  };
 
   return (
-    
     <Fragment>
       <ul className="ulPending">
         {currentOrdersList.map((order) => (
           <li
             className="pendingItem"
             key={order.orderId}
-            style={order.orderId===selectedOrder ? {backgroundColor : '#5D9B84'} : {backgroundColor : '#FFD195'}}            
+            style={
+              order.orderId === selectedOrder
+                ? { backgroundColor: "#5D9B84" }
+                : { backgroundColor: "#FFD195" }
+            }
             onClick={() => currentActiveOrder(order)}
           >
             <div> Customer: {order.data.customer} </div>
@@ -35,15 +34,18 @@ const PendingOrders = () => {
         ))}
       </ul>
 
-      
-        <SingleOrderContext.Provider value={[singleOrderData, setSingleOrderData]}>
-          
-          {singleOrderData === undefined ? <DefaultMessage /> : <Fragment><SinglePendingOrderComponent /> </Fragment>}
-          
-        </SingleOrderContext.Provider>
-      
-      </Fragment>
-     
+      <SingleOrderContext.Provider
+        value={[singleOrderData, setSingleOrderData]}
+      >
+        {singleOrderData === undefined ? (
+          <DefaultMessage />
+        ) : (
+          <Fragment>
+            <SinglePendingOrderComponent />{" "}
+          </Fragment>
+        )}
+      </SingleOrderContext.Provider>
+    </Fragment>
   );
 };
 
