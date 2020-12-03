aaeebdd3ddfa85f3327177613730809eff4db634
lalopez13 <63772079+lalopez13@users.noreply.github.com>
2020-12-03 16:51:44 -0500

    test files /Readme project

---
+++ src/components/item.test.js
@@ -0,0 +1,36 @@
+
+import React from "react";
+import { render, unmountComponentAtNode } from "react-dom";
+import { mount, configure, shallow } from "enzyme";
+import { act } from "react-dom/test-utils";
+import Item from "./item.jsx";
+import Adapter from "enzyme-adapter-react-16";
+
+configure({ adapter: new Adapter() });
+let container = null;
+beforeEach(() => {
+  // setup a DOM element as a render target
+  container = document.createElement("div");
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  // cleanup on exiting
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
+const fakeItem = {
+        "id": "hm-02",
+        "product": "Double burger",
+        "type": "Lunch",
+        "price": 15
+  };
+
+  
+    it("should render the item component", () => {
+   act( () => {
+        render(<Item product={fakeItem.product} price={fakeItem.price} />, container);
+      });
+      expect(container.querySelector("p").textContent).toBe(fakeItem.product);
+    });
